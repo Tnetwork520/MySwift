4aa8b05244083d94b92970343ec73e4cd5d0cd12
tnetwork <tnetwork.and.gear.txs.81jp@gmail.com>
2021-04-18 16:31:56 +0900

    Initial Commit

---
+++ MySwift/MySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftApp.swift
+//  MySwift
+//
+//  Created by Tnetwork on 2021/04/18.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
